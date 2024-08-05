ec791823e111bbce2ef0d1ec95d389aa9514a059
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-06 10:59:30 +1200

    testing of rounds
    Tested rounds of the game and the answers it outputs. Round numbers are working and all  lowercase answers work.

--- script.js
+++ script.js
@@ -6,17 +6,35 @@ let answers = [
 ]
 
 //Ask user what answer they want to choose
-function userAttempt() {
-let userAnswer = prompt ('paper, scissors, rock! Choose your answer wisely')
+function gameTime() {
+alert ("Let's play paper, scissors, rock! Best out of three?")
+
+
+let roundNumber = 1
+let userAnswer = ''
+let computerAnswer = ''
+while (roundNumber <= 3){
+    let userAnswer = prompt ('paper, scissors, rock! Choose your answer wisely')
 
 let index = Math.floor (Math.random()*3)
+let computerAnswer = answers[index]
 
-    alert('My answer is ' + answers[index])
+    alert('My answer is ' + computerAnswer)
 
-    while(userAnswer == 'scissors' && index == 'rock'){
-        alert ('I win! You lose...')
+    if(userAnswer == computerAnswer){
+        alert('Its a tie!')
+    } else if (userAnswer == 'scissors' && computerAnswer == 'paper'||
+              userAnswer == 'rock' && computerAnswer == 'scissors' ||
+               userAnswer == 'paper' && computerAnswer == 'rock'){
+     alert('You win!')
+    } else if (userAnswer == 'scissors' && computerAnswer == 'rock'||
+              userAnswer == 'rock' && computerAnswer == 'paper'||
+              userAnswer == 'paper' && computerAnswer == 'scissors'){
+     alert('You lose!')
     }
+    roundNumber = roundNumber + 1
 }
+ }
 
 
 
