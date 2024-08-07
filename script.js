bad8624a580052ed6e422db19d8b0af73f06ce7e
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-07 14:48:19 +1200

    counting points
    Tested for point tally/overall winner. Works well

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ alert ("Let's play paper, scissors, rock! Best out of three?")
 let roundNumber = 1
 let userAnswer = ''
 let computerAnswer = ''
+let computerWin = ''
+let userWin = ''
 while (roundNumber <= 3){
     let userAnswer = prompt ('paper, scissors, rock! Choose your answer wisely')
+    userAnswer = userAnswer.toLowerCase()
 
 let index = Math.floor (Math.random()*3)
 let computerAnswer = answers[index]
@@ -27,12 +30,22 @@ let computerAnswer = answers[index]
               userAnswer == 'rock' && computerAnswer == 'scissors' ||
                userAnswer == 'paper' && computerAnswer == 'rock'){
      alert('You win!')
+     userWin = userWin + 1
     } else if (userAnswer == 'scissors' && computerAnswer == 'rock'||
               userAnswer == 'rock' && computerAnswer == 'paper'||
               userAnswer == 'paper' && computerAnswer == 'scissors'){
      alert('You lose!')
+     computerWin = computerWin + 1
     }
     roundNumber = roundNumber + 1
+}
+
+if (userWin > computerWin){
+    alert('Congrajulations! You won.')
+} else if (userWin < computerWin){
+    alert ('Haha. You lose.')
+} else {
+    alert('Its a tie. Try again? ')
 }
  }
 
