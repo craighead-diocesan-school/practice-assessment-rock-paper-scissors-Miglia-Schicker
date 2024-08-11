b1dacfc8fd0c13e0e082e7a4f0ebcd79626fa2d8
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-12 09:20:07 +1200

    checking for valid and invalid answers
    Added in code to check if answers are valid or not - including capital letters
    Tested: paper. scissors, rock, PapEr, ScIsSoRs, ROCk
     - these answers worked in the code and the game continued as normal
    Tested: alien, pppaper, dolphin
    - these answers came up with the correct invalid attempt message

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ alert ("Let's play paper, scissors, rock! Best out of three?")
 let roundNumber = 1
 let userAnswer = ''
 let computerAnswer = ''
-let computerWin = ''
-let userWin = ''
+let computerWin = 0
+let userWin = 0
 while (roundNumber <= 3){
     let userAnswer = prompt ('paper, scissors, rock! Choose your answer wisely')
     userAnswer = userAnswer.toLowerCase()
 
+  
+
 let index = Math.floor (Math.random()*3)
 let computerAnswer = answers[index]
-
+if (answers.includes(userAnswer)){
     alert('My answer is ' + computerAnswer)
+   } else {
+    alert ('That is and invalaid answer. Try again with paper, scissors, or rock')
+   }
+    
 
     if(userAnswer == computerAnswer){
         alert('Its a tie!')
@@ -36,16 +42,16 @@ let computerAnswer = answers[index]
               userAnswer == 'paper' && computerAnswer == 'scissors'){
      alert('You lose!')
      computerWin = computerWin + 1
-    }
+    } 
     roundNumber = roundNumber + 1
 }
 
 if (userWin > computerWin){
-    alert('Congrajulations! You won.')
+    alert('Congratulations! You won. ' + userWin + ':' + computerWin)
 } else if (userWin < computerWin){
-    alert ('Haha. You lose.')
+    alert ('Haha. You lose. ' + userWin + ':' + computerWin)
 } else {
-    alert('Its a tie. Try again? ')
+    alert('Its a tie. Try again? ' + userWin + ':' + computerWin)
 }
  }
 
